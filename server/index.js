76aa8897c5decac15c24521e39967b33adeaa6ec
EmanuelPRA <emanuel.pracic@gmail.com>
2022-08-23 13:34:15 +0200

    Backend added for the current SOP

--- server/index.js
+++ server/index.js
@@ -39,9 +39,6 @@ app.post('/postinsert', (req, res) =>{
     })
 })
 
-
-
-
 app.get('/check:id', (req, res) =>{
     const ident = req.params.id
     db.query("SELECT * FROM users WHERE userid='"+ [req.params.id]+"'", (err, result) =>{
@@ -56,7 +53,7 @@ app.get('/check:id', (req, res) =>{
             console.log(err)
         }
     })
-})//somebody please explain why when it return null it doesn't function on the front end
+})
 
 app.post('/create', (req, res) => {
     const filePath = "/home/arlemar/Documents/reactcrum/client/src/img/" + req.files.file.name 
@@ -96,8 +93,35 @@ app.get('/feed', (req, res) =>{
     })
 })
 
+app.get('/comments:postid', (req, res) =>{
+    db.query("SELECT (comment_body, commentor_id) WHERE post_id = (?) FROM comments ORDER BY post_time", [req.params.postid], (err, result) => {
+        if(result.length !== 0){
+            console.log(result)
+            res.send(result)
+        }else{
+            console.log("null:" + result)
+            res.send(false)
+        }
+        if(err){
+            console.log(err)
+        }
+    })
+})
 
-app.post('/userdelete', (req, res) =>{
+app.post('/commentpost', (req, res) =>{
+    const postId = req.body.postid
+    const commentorId = req.body.commentorid
+    const commentBody = req.body.commentbody
+    db.query("INSERT INTO comments (post_id, commentor_id, comment_body) VALUES (?,?,?)", [postId, commentorId, commentBody], (err, result) =>{
+        if(err){
+            console.log(err)
+        }else{
+            console.log("Post Commented")
+        }
+    })
+})
+
+app.post('/deleteuser', (req, res) =>{
     const ident = req.body.id
     db.query("DELETE FROM users WHERE userid='"+ [req.body.id]+"'", (err, result) =>{
         if(err){
