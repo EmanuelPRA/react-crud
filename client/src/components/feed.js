520bab274e79a08bb4ae83087605d04160b0627f
EmanuelPRA <emanuel.pracic@gmail.com>
2022-08-24 11:04:37 +0200

    second attempt at rendering feed(not functional)

--- client/src/components/feed.js
+++ client/src/components/feed.js
@@ -9,20 +9,19 @@ export const Feed = () =>{
 
 
         const object = Object.values(JSON.parse(JSON.stringify(res)))
-        console.log(object[0])
           
          posts = object[0]
-         console.log(posts)
+         if(posts.length > 1){
+          posts.map((anObjectMapped, index) => {
+            
+            return (
+                <p key={index}>
+                    {anObjectMapped}
+                </p>
+            );
+            }
+          )}
         
     });
     
-    if(posts.length > 1){
-    console.log(posts)
-    posts.map((anObjectMapped, index) => {
-      return (
-          <p key={`${anObjectMapped.postText}_{anObjectMapped.email}`}>
-              {anObjectMapped.post_time} - {anObjectMapped.posterid}
-          </p>
-      );
-      }
-    )}}
+    }
