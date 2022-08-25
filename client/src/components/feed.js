d12f0e79370104849ec7817313c4babcbde5ec14
EmanuelPRA <emanuel.pracic@gmail.com>
2022-08-25 13:29:06 +0200

    displaying feed FINALLY ADDED

--- client/src/components/feed.js
+++ client/src/components/feed.js
@@ -2,26 +2,33 @@ import Axios from 'axios';
 import React, {useState} from 'react';
 
 export const Feed = () =>{
-    let posts = [{}]
 
-    Axios.get('http://localhost:3001/feed', {
-      }).then((res) =>{
+  const [post, setPost] = React.useState(null);
 
-
-        const object = Object.values(JSON.parse(JSON.stringify(res)))
-          
-         posts = object[0]
-         if(posts.length > 1){
-          posts.map((anObjectMapped, index) => {
-            
-            return (
-                <p key={index}>
-                    {anObjectMapped}
-                </p>
-            );
-            }
-          )}
-        
+  React.useEffect(() => {
+    Axios.get("http://localhost:3001/feed").then((response) => {
+      setPost(response.data);
     });
+  }, []);
+
+  if (!post) return null;
+
+  return (
+
+    <div className='feed'>
+      
+      {post.map(({id, posterid, post_text, post_image, post_time}) => {
+        return (
+          <div key={id} className="post">
+            <img alt={"post #"+ id} src={require('/home/arlemar/Documents/reactcrum/client/src/img/' + post_image)}/>
+            <p>{post_text}</p>
+            <p>Posted By:{posterid}</p>
+            <p>Posted On:{post_time}</p>
+          </div>
+        );
+      })}
+
+    </div>
+  );
     
-    }
+}
